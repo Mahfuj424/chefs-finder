9fc1b3095235d417efe0e106b138d7dcf8f770ce
mahfuj <mahfujahmad424@gmail.com>
2023-05-18 14:58:55 +0600

    first commit

--- src/components/PrivetRoute.jsx
+++ src/components/PrivetRoute.jsx
@@ -3,14 +3,20 @@ import { AuthContext } from './AuthProvider';
 import { Navigate, useLocation } from 'react-router-dom';
 
 const PrivetRoute = ({children}) => {
-    const { user } = useContext(AuthContext);
+    const { user, loading } = useContext(AuthContext);
     
     const location = useLocation();
     console.log(location);
     
+    if (loading) {
+        return
+    }
+
     if (user) {
         return children;
     }
+
+    
 
     return (
         <Navigate to='/login' state={{ from: location }} replace ></Navigate>
